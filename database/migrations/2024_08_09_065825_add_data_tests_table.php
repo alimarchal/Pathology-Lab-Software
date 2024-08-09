0cf5dadfa0d3e318196bda3e0966abe1c24952b2
Mehar <lovefist43@gmail.com>
2024-08-09 12:03:01 +0500

    version 1.12

---
+++ database/migrations/2024_08_09_065825_add_data_tests_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tests', function (Blueprint $table) {
+            $table->string('registeration_time')->nullable()->after('description');
+            $table->string('collection_time')->nullable()->after('description');
+            $table->string('reporting_time')->nullable()->after('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tests', function (Blueprint $table) {
+            //
+        });
+    }
+};
